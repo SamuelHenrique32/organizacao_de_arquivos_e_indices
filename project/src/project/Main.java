f6a1794d3b4c460a0070dc16a4e33a996ba46e35
Samuel Dalmas <samuel.dalmas@mobitec.com.br>
2020-10-12 16:12:46 -0300

    Updating Line Size

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -2,9 +2,11 @@ package project;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 /*
  * Line 22888 is the bigger, length = 800
@@ -18,6 +20,10 @@ public class Main {
 		
 		BufferedReader myBuffer = new BufferedReader(new InputStreamReader(new FileInputStream("../Data/consulta_cand_2020_RS.csv"), "ISO-8859-1"));
 
+		OutputStream outputStream = new FileOutputStream("../Data/saida.txt");
+
+		StringBuilder strBuilder = null;
+		
 		String linha = myBuffer.readLine();
 
 		while(linha != null) {
@@ -29,6 +35,18 @@ public class Main {
 			if(linha.length() == 800) {
 				System.out.println(linha);
 			}
+
+			strBuilder = new StringBuilder(linha);
+			
+			for(int i=linha.length() ; i<799 ; i++) {
+				strBuilder.append(" ");
+			}
+			
+			strBuilder.append("\n");
+					
+			outputStream.write(strBuilder.toString().getBytes());
+				
+			outputStream.flush();			
 
 			linha = myBuffer.readLine();
 		}
