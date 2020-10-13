6a60adda6475d72d4a496f1d81f0b70585e0cbc7
Samuel Dalmas <samuel.dalmas@mobitec.com.br>
2020-10-12 21:44:14 -0300

    Read and Show File

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,58 +1,99 @@
 package project;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-
-/*
- * Line 22888 is the bigger, length = 800
- * */
+import java.io.RandomAccessFile;
+import java.util.Scanner;
 
 public class Main {
 
+	private static final int kOPC_MOSTRAR_DADOS = 1;
+	private static final int kOPC_PESQUISA_BINARIA = 2;
+
+	private static final int kINVALID_VALUE = -1;
+
+	private static final int kREGISTER_LINE_SIZE = 800;
+
+	private static final int kQTD_REGISTERS = 10000;
+
+	private static final String kREGISTERS_FILE_NAME = "../Data/consulta_cand_2020_RS.csv";
+
 	public static void main(String[] args) throws IOException {
-		
-		int maxSize = -1;
-		
-		BufferedReader myBuffer = new BufferedReader(new InputStreamReader(new FileInputStream("../Data/consulta_cand_2020_RS.csv"), "ISO-8859-1"));
 
-		OutputStream outputStream = new FileOutputStream("../Data/saida.txt");
+		int opc = kINVALID_VALUE;
 
-		StringBuilder strBuilder = null;
-		
-		String linha = myBuffer.readLine();
+		RandomAccessFile randomAccessFile = null;
 
-		while(linha != null) {
-			
-			if(linha.length() > maxSize) {
-				maxSize = linha.length();
-			}
-			
-			if(linha.length() == 800) {
-				System.out.println(linha);
-			}
+		Scanner sc = new Scanner(System.in);
 
-			strBuilder = new StringBuilder(linha);
-			
-			for(int i=linha.length() ; i<799 ; i++) {
-				strBuilder.append(" ");
-			}
-			
-			strBuilder.append("\n");
-					
-			outputStream.write(strBuilder.toString().getBytes());
-				
-			outputStream.flush();			
+		try {
+			randomAccessFile = new RandomAccessFile(kREGISTERS_FILE_NAME, "rw");	
 
-			linha = myBuffer.readLine();
+		} catch (Exception e) {
+
+			System.out.println("Nao foi possivel abrir o arquivo de registros");
 		}
+		
 
-		System.out.println(maxSize);
+		while(true){
 
-		myBuffer.close();
+			showMenu();
+
+			opc = sc.nextInt();
+
+			sc.close();
+
+			switch(opc){
+			
+				case kOPC_MOSTRAR_DADOS:
+
+					System.out.println();
+
+					showAllRegisters(randomAccessFile);
+
+				break;
+
+				case kOPC_PESQUISA_BINARIA:
+					
+				break;
+			}
+		}		
+	}
+	
+	public static void showMenu() {
+		System.out.println("\n\nOpcoes:");
+		System.out.println("1- Mostrar Dados");
+		System.out.println("2- Pesquisa Binaria");
+		System.out.println("3- Sair");
+		System.out.print("Opcao escolhida: ");
+	}
+
+	public static void showAllRegisters(RandomAccessFile randomAccessFile) throws IOException {
+
+		char[] charReadLine = new char[kREGISTER_LINE_SIZE];
+
+		int lineCount = 0, seekPos = 0, i = 0;
+
+		String currentLine = null;
+
+		randomAccessFile.seek(seekPos);
+
+		while(lineCount < kQTD_REGISTERS)
+		{
+			for(i=0 ; i<kREGISTER_LINE_SIZE ; i++) {
+
+				charReadLine[i] = (char)randomAccessFile.readByte();
+	        	
+			}
+	        
+	        currentLine = new String(charReadLine);
+	        
+	        System.out.print(currentLine);
+
+	        seekPos += kREGISTER_LINE_SIZE;
+
+	        randomAccessFile.seek(seekPos);
+
+	        lineCount++;
+		}		
 	}
 }
